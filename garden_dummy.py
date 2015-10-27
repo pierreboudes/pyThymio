39f38ef494102c5d53c858e341f6eb387d42730b
boudes <boudes@lipn.univ-paris13.fr>
2015-10-27 22:57:46 +0100

    accelerometer in the garden

---
+++ garden_dummy.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pythymio
+import random
+
+
+def tg():
+    """ dummy tournergauche
+    """
+    print 'tg'
+
+def td():
+    """ dummy tournerdroite
+    """
+    print 'td'
+
+def av():
+    """ dummy avance
+    """
+    print 'av'
+
+def ra():
+    """ dummy ramasse
+    """
+    print 'ra'
+
+def dp():
+    """ dummy depose
+    """
+    print 'dp'
+
+
+with pythymio.thymio(["acc"],[]) as Thym:
+
+    state = dict([])
+    state["time"] = 0
+    state["delay"] = 10
+
+    def dispatch(evtid, evt_name, evt_args):
+        global state
+
+        # https://www.thymio.org/en:thymioapi prox freq is 16Hz
+        if evt_name == "fwd.acc": # every 0.0625 sec
+            state["time"] += 0.0625
+            state["delay"] -= 1
+            if state["delay"] < 0:
+                if  7 < evt_args[1] < 14:
+                    if evt_args[0] > 10:
+                        state["delay"] = 20
+                        tg()
+                    elif evt_args[0] < -10:
+                        state["delay"] = 20
+                        td()
+                elif evt_args[1] > 20 and abs(evt_args[0]) < 8:
+                    state["delay"] = 10
+                    av()
+                elif  evt_args[1] < 5:
+                    if evt_args[0] > 10:
+                        state["delay"] = 20
+                        dp()
+                    elif evt_args[0] < -10:
+                        state["delay"] = 20
+                        ra()
+
+        else: # Wat?
+            print evt_name
+
+    # Now lets start the loopy thing
+    Thym.loop(dispatch)
+    print "state is %s" % state
+    print "Sayonara"
---
+++ garden_real.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pythymio
+import random
+
+from gardenworld import *
+init('info2_1')
+
+with pythymio.thymio(["acc"],[]) as Thym:
+
+    state = dict([])
+    state["time"] = 0
+    state["delay"] = 10
+
+    def dispatch(evtid, evt_name, evt_args):
+        global state
+
+        # https://www.thymio.org/en:thymioapi prox freq is 16Hz
+        if evt_name == "fwd.acc": # every 0.0625 sec
+            state["time"] += 0.0625
+            state["delay"] -= 1
+            if state["delay"] < 0:
+                if  7 < evt_args[1] < 14:
+                    if evt_args[0] > 10:
+                        state["delay"] = 20
+                        tg()
+                    elif evt_args[0] < -10:
+                        state["delay"] = 20
+                        td()
+                elif evt_args[1] > 20 and abs(evt_args[0]) < 8:
+                    state["delay"] = 10
+                    av()
+                elif  evt_args[1] < 5:
+                    if evt_args[0] > 10:
+                        state["delay"] = 20
+                        dp()
+                    elif evt_args[0] < -10:
+                        state["delay"] = 20
+                        ra()
+
+        else: # Wat?
+            print evt_name
+
+    # Now lets start the loopy thing
+    Thym.loop(dispatch)
+    print "state is %s" % state
+    print "Sayonara"
